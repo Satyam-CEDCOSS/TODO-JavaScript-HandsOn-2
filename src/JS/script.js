926e07041b014d7bff2aa3bad7ff24a944c83cb2
satyam01032023 <satyambajpai@cedcoss.com>
2023-03-14 11:22:45 +0530

    Stand in Line

--- src/JS/script.js
+++ src/JS/script.js
@@ -173,3 +173,20 @@ function processArg(num) {
 }
 // Only change code below this line
 processed = processArg(7)
+
+// 28 Stand in Line
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    const removed = arr.shift();
+    return removed;
+    // Only change code above this line
+  }
+  
+  // Setup
+  let testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
