239a1020e2ed801173a50f06ffda2cb981f53589
satyam01032023 <satyambajpai@cedcoss.com>
2023-03-14 10:11:31 +0530

    Appending Variables to Strings

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,4 +1,9 @@
-// Constructing Strings with Variables
+// 1 Constructing Strings with Variables
 const myName = " Satyam Bajpai ";
 const myStr = "My name is"+ myName +"and I am well";
+ 
+// 2 Appending Variables to Strings
+const someAdjective = "Fun";
+let myStr = "Learning to code is ";
+myStr += someAdjective
 
