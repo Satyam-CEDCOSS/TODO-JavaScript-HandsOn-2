8d5b8c970bdad473699abc38b92c9f8faaf45654
satyam01032023 <satyambajpai@cedcoss.com>
2023-03-14 11:16:22 +0530

    Assignment with a Returned Value

--- src/JS/script.js
+++ src/JS/script.js
@@ -165,3 +165,11 @@ function addFive() {
 addThree();
 addFive();
 
+// 27 Assignment with a Returned Value
+// Setup
+let processed = 2;
+function processArg(num) {
+  return (num + 3) / 5;
+}
+// Only change code below this line
+processed = processArg(7)
