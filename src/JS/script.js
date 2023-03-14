686a255255815f8e180cf74b7aa408628f511783
satyam01032023 <satyambajpai@cedcoss.com>
2023-03-14 11:27:32 +0530

    Use Conditional Logic with If Statements

--- src/JS/script.js
+++ src/JS/script.js
@@ -197,3 +197,13 @@ function welcomeToBooleans() {
     return true; // Change this line
     // Only change code above this line
   }
+
+// 30 Use Conditional Logic with If Statements
+function trueOrFalse (myCondition) {
+    if (myCondition) {
+      return "Yes, that was true";
+    }
+    return "No, that was false";
+  }
+  trueOrFalse(true);
+  trueOrFalse(false);
