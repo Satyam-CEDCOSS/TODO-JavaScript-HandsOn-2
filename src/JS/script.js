709df272de636919824d93990ad1c4a6d1f1e01c
satyam01032023 <satyambajpai@cedcoss.com>
2023-03-14 10:39:42 +0530

    Access Multi-Dimensional Arrays With Indexes

--- src/JS/script.js
+++ src/JS/script.js
@@ -58,3 +58,13 @@ let myData = myArray[0]
 // 13 Modify Array Data With Indexes
 const myArray = [18, 64, 99];
 myArray[0] = 45
+
+// 14 Access Multi-Dimensional Arrays With Indexes
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+const myData = myArray[2][1];
+
