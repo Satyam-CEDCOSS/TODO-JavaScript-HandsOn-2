9cd5ceb84dd1a957c7dcce7e4b61efffd573fd12
satyam01032023 <satyambajpai@cedcoss.com>
2023-03-14 11:11:19 +0530

    Global vs. Local Scope in Functions

--- src/JS/script.js
+++ src/JS/script.js
@@ -139,3 +139,15 @@ function myLocalScope() {
   // Run and check the console
   // myVar is not defined outside of myLocalScope
   console.log('outside myLocalScope', myVar);
+
+// 25 Global vs. Local Scope in Functions
+// Setup
+const outerWear = "T-Shirt";
+function myOutfit() {
+  // Only change code below this line
+  let outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+myOutfit();
+
